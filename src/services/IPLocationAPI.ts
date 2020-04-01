14b82cae12ffbfcd34087f6f34d0befaab69366f
Kyle Abughanem <kyle.abughanem@gmail.com>
2020-04-01 08:42:07 -0700

    fix http for ip address

--- src/services/IPLocationAPI.ts
+++ src/services/IPLocationAPI.ts
@@ -2,18 +2,18 @@ import axios from 'axios'
 import NewLetter from '../components/NewLetter';
 
 export interface UserLocation {
-  lat: number,
-  lon: number,
-  country: string,
-  countryCode: string,
-  regionName: string,
-  city: string,
+  lat: number;
+  lon: number;
+  country: string;
+  countryCode: string;
+  regionName: string;
+  city: string;
 }
 
 class IPLocationAPI {
 
   static getLocationFromIP(): Promise<UserLocation> {
-    return axios.get('//ip-api.com/json/').then((result) => {
+    return axios.get('http://ip-api.com/json/').then((result) => {
       const data: UserLocation = result.data
       return data
     })
