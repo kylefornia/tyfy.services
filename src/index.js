5bf847b98afaf62f6ed4f7d0c58ff1fdeeb838ec
Kyle Abughanem <kyle.abughanem@gmail.com>
2020-11-01 17:29:11 -0800

    PWA + firebase offline

--- src/index.js
+++ src/index.js
@@ -14,4 +14,14 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register();
+serviceWorker.register({
+  onUpdate: registration => {
+    // alert('New version available!  Ready to update?');
+    if (registration && registration.waiting) {
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+    }
+    window.location.reload();
+    //changed
+  }
+});
